609786cde15c1f6938afdf12a7756fc6679f5818
Bernard Wong <ben.wc88@gmail.com>
2019-11-29 20:41:34 +0000

    Added test users

--- src/main/java/ie/gmit/ds/UserDatabase.java
+++ src/main/java/ie/gmit/ds/UserDatabase.java
@@ -7,6 +7,15 @@ import java.util.List;
 public class UserDatabase {
     private static HashMap<Integer, User> usersMap = new HashMap<>();
 
+    public UserDatabase() {
+        User user1 = new User(1, "test1", "test@email.com", "testpw");
+        User user2 = new User(2, "testsubject", "testmail@email.com", "password123");
+        User user3 = new User(3, "Bernard", "emailtest@email.com", "pw512");
+        usersMap.put(user1.getUserId(), user1);
+        usersMap.put(user2.getUserId(), user2);
+        usersMap.put(user3.getUserId(), user3);
+    }
+
     public static List<User> getUsers() {
         return new ArrayList<>(usersMap.values());
     }
